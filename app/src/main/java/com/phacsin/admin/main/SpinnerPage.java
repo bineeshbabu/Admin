9fa357b3c8bf129507d597e2e0fedcb3a2a87b56
bineeshbabupattat <bineeshbabupattat@gmail.com>
2016-09-23 13:55:49 +0530

    lot n lot changes. :-0

--- app/src/main/java/com/phacsin/admin/main/SpinnerPage.java
+++ app/src/main/java/com/phacsin/admin/main/SpinnerPage.java
@@ -1,7 +1,10 @@
 package com.phacsin.admin.main;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,20 +19,20 @@ import com.phacsin.admin.scan.ScanRegisterEvent;
  * Created by Bineesh P Babu on 21-09-2016.
  */
 public class SpinnerPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    Spinner spinnervenue,spinnerevent,spinnereventdetail;
-    private String[] venues = { "Venue 1", "Venue 2", "Venue 3", "Venue 4",
-            "Venue 5", "Venue 6", "Venue 7", "Venue 8"};
-    private String[] events = { "Workshops", "Panel Discussions", "Competitions", "Talks",};
-    private String[] event_details = { "i2p", "bplan", "bullsnabears", "craetive ",};
+    Spinner spinnervenue,spinnereventdetail;
+    private String[] venues = { "Venue 1", "Venue 2", "Venue 3", "Venue 4","Venue 5", "Venue 6", "Venue 7", "Venue 8"};
+    private String[] event_details = { "Rise Of The Phoenix", "Motivate", "Investing Wisely", "Entrepreneurship In IOT ","Prototype To Product","Inspire"
+    ,"Bulls N Bears","Corporate Roadies","Idea To Product","Best E Cell","Clash Of Corporates","Kickstarter","B-plan","Best Critic"
+    ,"On Your Marks","Hire The Best","Lean Startup","Binary Marketing","Creative Thinking","Entrepreneurship In Non-IT Sector","Pitch Perfect"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spinner);
         spinnervenue = (Spinner) findViewById(R.id.venue);
-        spinnerevent = (Spinner) findViewById(R.id.events);
         spinnereventdetail = (Spinner) findViewById(R.id.event_detail);
         Button apply=(Button)findViewById(R.id.btn_apply) ;
+
         apply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,9 +45,6 @@ public class SpinnerPage extends AppCompatActivity implements AdapterView.OnItem
                 android.R.layout.simple_spinner_item, venues);
         venue_list.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> event_list = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, events);
-        event_list.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         ArrayAdapter<String> event_details_list = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, event_details);
@@ -53,9 +53,6 @@ public class SpinnerPage extends AppCompatActivity implements AdapterView.OnItem
         spinnervenue.setAdapter(venue_list);
         spinnervenue.setOnItemSelectedListener(this);
 
-        spinnerevent.setAdapter(event_list);
-        spinnerevent.setOnItemSelectedListener(this);
-
         spinnereventdetail.setAdapter(event_details_list);
         spinnereventdetail.setOnItemSelectedListener(this);
     }
@@ -63,8 +60,6 @@ public class SpinnerPage extends AppCompatActivity implements AdapterView.OnItem
         spinnervenue.setSelection(position);
         String selState = (String) spinnervenue.getSelectedItem();
 
-        spinnerevent.setSelection(position);
-        String sel1 = (String) spinnerevent.getSelectedItem();
 
         spinnereventdetail.setSelection(position);
         String sel2 = (String) spinnereventdetail.getSelectedItem();
